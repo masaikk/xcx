c548403b7d3381867cf2a771fe3ee5f2f8f4d9c9
masaikk <201830660420@mail.scut.edu.cn>
2020-07-16 21:32:56 +0800

    7/16 V3.0

--- pages/hx/hx.js
+++ pages/hx/hx.js
@@ -8,15 +8,148 @@ Page({
     strsrc:"http://119.23.182.180/hexie/au/ready.wav",
     ausrc:"http://119.23.182.180/hexie/au/2.wav",
     endsrc:"http://119.23.182.180/hexie/au/end.wav",
+    autitle:'',
+    autype:1,
+    auans:'',
+    mess:'交替使用远近光灯的项目，远近光之间至少需要交替2次以上\n',
+    aulist:[
+      {
+        'title':'在路边临时停车',
+        'ans':'开启小灯（示宽灯）和危险警示灯（双闪）',
+        'anstype':1
+      },
+      {
+        'title':'进入无照明，照明不良的道路行驶时',
+        'ans':'打开远光灯',
+        'anstype':2
+      },
+      {
+        'title':'准备会车',
+        'ans':'打开近光灯',
+        'anstype':3
+      },
+      {
+        'title':'准备通过路口',
+        'ans':'打开近光灯',
+        'anstype':3
+      },
+      {
+        'title':'请打开近光灯',
+        'ans':'打开近光灯',
+        'anstype':3
+      },
+      {
+        'title':'同方向近距离跟车行驶',
+        'ans':'打开近光灯',
+        'anstype':3
+      },
+      {
+        'title':'在有路灯，照明良好的道路上行驶时',
+        'ans':'打开近光灯',
+        'anstype':3
+      },
+      {
+        'title':'准备超车',
+        'ans':'交替使用远近光灯，灯光停留在近光灯',
+        'anstype':4
+      },
+      {
+        'title':'前方通过急弯',
+        'ans':'交替使用远近光灯，灯光停留在近光灯',
+        'anstype':4
+      },
+      {
+        'title':'前方通过坡路',
+        'ans':'交替使用远近光灯，灯光停留在近光灯',
+        'anstype':4
+      },
+      {
+        'title':'前方通过拱桥',
+        'ans':'交替使用远近光灯，灯光停留在近光灯',
+        'anstype':4
+      },
+      {
+        'title':'前方通过人行横道',
+        'ans':'交替使用远近光灯，灯光停留在近光灯',
+        'anstype':4
+      },
+      {
+        'title':'前方通过没有交通信号灯控制的路口',
+        'ans':'交替使用远近光灯，灯光停留在近光灯',
+        'anstype':4
+      },
 
+    ]
+
+  },
+  anstap(e){
+    //console.warn(e.currentTarget.id);
+    switch(e.currentTarget.id){
+      case 'a1': {
+        if(this.data.autype==2){
+          wx.showToast({
+            title: '正确',
+          })}
+        else{
+          wx.showToast({
+            title: '错误',
+          })
+        }
+        break;
+      };
+      case 'a2': {
+        if(this.data.autype==3){
+          wx.showToast({
+            title: '正确',
+          })}
+        else{
+          wx.showToast({
+            title: '错误',
+          })
+        }
+        break;
+      }
+      case 'a3': {
+        if(this.data.autype==4){
+          wx.showToast({
+            title: '正确',
+          })}
+        else{
+          wx.showToast({
+            title: '错误',
+          })
+        }
+        break;
+      }
+      case 'a4': {
+        if(this.data.autype==1){
+          wx.showToast({
+            title: '正确',
+          })}
+        else{
+          wx.showToast({
+            title: '错误',
+          })
+        }
+        break;
+      }
+
+      default: console.warn(e.currentTarget.id);break;
+    };
   },
   change(e){
     let aunum = Math.floor(Math.random() * (14 - 1))+1;
     this.setData({
       ausrc:"http://119.23.182.180/hexie/au/"+aunum+".wav",
+      autype:this.data.aulist[aunum-1].anstype,
+      autitle:this.data.aulist[aunum-1].title,
+      auans:this.data.aulist[aunum-1].ans,
     });
+    // console.warn(this.data.autype);
+    // console.warn(this.data.autitle);
     wx.showToast({
       title: '更换成功',
+      
     })
   },
 
