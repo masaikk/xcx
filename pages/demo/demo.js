de2c2d895aeedaf0009da48b0dc030998c3eea65
masaikk <201830660420@mail.scut.edu.cn>
2020-07-16 11:22:48 +0800

    7/16 V1.0

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -1,8 +1,9 @@
 //Page Object
 Page({
   data: {
-    imgnum:2,
-    imgsrc:"http://119.23.182.180/project/jp2.jpg",
+    ausrc:"http://119.23.182.180/project/ch.wav",
+    imgnum:1,
+    imgsrc:"http://119.23.182.180/project/jp1.jpg",
     message:"424324234324",
     poe:{
       name:"saisnas",
@@ -33,6 +34,15 @@ Page({
 
     
   },
+  click(e){
+    wx.showToast({
+      title: '更换为日语',
+    });
+    this.setData({
+      ausrc:"http://119.23.182.180/project/jp.wav"
+    });
+  }
+  ,
   inputHandle(e){
     //console.warn(e.detail.value)
     //this.message=e.detail.value;
@@ -42,7 +52,7 @@ Page({
   },
   showInfo(e){
     this.setData({
-      imgnum:((this.data.imgnum+1)>4?2:this.data.imgnum+1 ),
+      imgnum:((this.data.imgnum+1)>4?1:this.data.imgnum+1 ),
       imgsrc:"http://119.23.182.180/project/jp"+this.data.imgnum+".jpg",
     })
   },
